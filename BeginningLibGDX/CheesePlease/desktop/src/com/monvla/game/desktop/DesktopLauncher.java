5f50246d25d15e4c3196f424e8f12a58dbb064c7
Monakhov Vladimir <monvla16@gmail.com>
2019-07-07 19:52:10 +0300

    cheese please

---
+++ BeginningLibGDX/CheesePlease/desktop/src/com/monvla/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.monvla.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.monvla.game.CheesePleaseGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CheesePleaseGame(), config);
+	}
+}
